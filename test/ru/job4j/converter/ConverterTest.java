98b12aaae9c2b6647c7c86dc7b9bac6ab8cbace0
kuzurik <vorota-24@bk.ru>
2021-05-26 10:07:30 +0300

    1. Модульные тесты. [#159435 #160731]

--- test/ru/job4j/converter/ConverterTest.java
+++ test/ru/job4j/converter/ConverterTest.java
@@ -1,15 +1,17 @@
 package ru.job4j.converter;
 
-import junit.framework.TestCase;
 import org.junit.Assert;
+import org.junit.Test;
 
-public class ConverterTest extends TestCase {
+public class ConverterTest {
 
+    @Test
     public void testRubleToEuro() {
         int result = Converter.rubleToEuro(180);
         Assert.assertEquals(2, result);
     }
 
+    @Test
     public void testRubleToDollar() {
         int result = Converter.rubleToDollar(148);
         Assert.assertEquals(2, result);
