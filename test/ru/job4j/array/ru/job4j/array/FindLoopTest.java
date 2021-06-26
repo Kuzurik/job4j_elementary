86ba06142ebdeac803957e72e72c3fa9831b2970
kuzurik <vorota-24@bk.ru>
2021-06-26 21:28:45 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

---
+++ test/ru/job4j/array/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,47 @@
+package ru.job4j.array.ru.job4j.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHasLength5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 5;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 0;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenElHas9ThenMinusOne() {
+        int[] data = new int[] {5, 10, 3};
+        int el = 9;
+        int result = FindLoop.indexOf(data, el);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenStar2Finish4El2Then3() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenStar2Finish4El9ThenMinusOne() {
+        int[] data = new int[] {5, 2, 10, 2, 4};
+        int el = 9;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
+    }
+}
